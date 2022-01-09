549e740a6ed625c7fc9e8c08d6bc3163628de315
Ashish Bhosale <ashish.bhosale@capgemini.com>
2022-01-09 18:00:39 +0530

    first commit

---
+++ Models/Login.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactApplication.Models
+{
+	public class Login{
+        [Key]
+    public int UserId {get;set;}
+    public string Email {get;set;}
+    public string Password{get;set;}
+    [ForeignKey("UserRegId")]
+    public Register Register {get;set;}
+    }
+    
+    
+}
